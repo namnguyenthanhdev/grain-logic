2cff0bad0e1ff70d5d45d92c25d2214b0ee92b2d
namthanh <nam.nguyenthanh@doltech.vn>
2023-09-28 10:09:25 +0700

    fix: [Project] be able to render style every reload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import type { AppProps } from "next/app";
-import '../styles/globals.css';
+import "../styles/globals.css";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
